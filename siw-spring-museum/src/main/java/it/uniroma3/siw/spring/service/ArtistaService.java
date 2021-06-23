a0ac88a17f9ff0b8a08b1c06862d850fd1cf55dd
matteo-carnebella <mat.carnebella@gmail.com>
2021-06-23 18:08:32 +0200

    siw-museum/23-06-2021
    Progetto museo aggiornato al 23/06/2021

---
+++ siw-spring-museum/src/main/java/it/uniroma3/siw/spring/service/ArtistaService.java
@@ -0,0 +1,58 @@
+package it.uniroma3.siw.spring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.spring.model.Artista;
+import it.uniroma3.siw.spring.repository.ArtistaRepository;
+
+
+@Service
+public class ArtistaService {
+
+	@Autowired
+	private ArtistaRepository artistaRepository;
+	
+	@Transactional
+	public Artista inserisci(Artista artista) {
+		return artistaRepository.save(artista);
+	}
+	
+	@Transactional
+	public List<Artista> tutti() {
+		return (List<Artista>)artistaRepository.findAll();
+	}
+	
+	@Transactional
+	public Artista artistaPerId(Long id) {
+		Optional<Artista> optional = artistaRepository.findById(id);
+		if (optional.isPresent())
+			return optional.get();
+		else 
+			return null;
+	}
+	
+	@Transactional
+	public List<Artista> artistaPerNomeAndCognome(String nome, String cognome) {
+		return artistaRepository.findByNomeAndCognome(nome, cognome);
+	}
+	
+	@Transactional
+	public List<Artista> artistaPerNazione(String nazione){
+		return artistaRepository.findByNazione(nazione);
+	}
+	
+	@Transactional
+	public boolean alreadyExists(Artista artista) {
+		List<Artista> artisti = this.artistaRepository.findByNomeAndCognome(artista.getNome(), artista.getCognome());
+		if (artisti.size() > 0)
+			return true;
+		else 
+			return false;
+	}
+	
+}
